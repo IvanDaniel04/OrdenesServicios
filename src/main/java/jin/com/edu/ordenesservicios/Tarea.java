90086d1312a466f2247e121d167c390966de5486
Daniel Ivan Estrada Morando <125703648+IvanDaniel04@users.noreply.github.com>
2023-03-23 20:39:50 -0600

    ModificarOrdenFuncional
    Realicé la interfaz de modificar orden para que sea totalmente funcional y se puedan editar las ordenes de servicio.

---
+++ src/main/java/jin/com/edu/ordenesservicios/Tarea.java
@@ -0,0 +1,79 @@
+package jin.com.edu.ordenesservicios;
+
+import java.util.Date;
+
+public class Tarea {
+    private int id;
+    private String tiempoEstimado;
+    private String descripción;
+    private String observacion;
+    private Date fechaFinalización;
+    private Date fechaRegistro;
+    private String estado;
+
+    public Tarea(String estado){
+        this.estado = estado;
+    }
+
+    public Tarea(int id, String tiempoEstimado, String operador, String descripción, String observacion, Date fechaFinalización, Date fechaRegistro) {
+        this.id = id;
+        this.tiempoEstimado = tiempoEstimado;
+        this.descripción = descripción;
+        this.observacion = observacion;
+        this.fechaFinalización = fechaFinalización;
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTiempoEstimado() {
+        return tiempoEstimado;
+    }
+
+    public void setTiempoEstimado(String tiempoEstimado) {
+        this.tiempoEstimado = tiempoEstimado;
+    }
+
+    public String getDescripción() {
+        return descripción;
+    }
+
+    public void setDescripción(String descripción) {
+        this.descripción = descripción;
+    }
+
+    public String getObservacion() {
+        return observacion;
+    }
+
+    public void setObservacion(String observacion) {
+        this.observacion = observacion;
+    }
+
+    public Date getFechaFinalización() {
+        return fechaFinalización;
+    }
+
+    public void setFechaFinalización(Date fechaFinalización) {
+        this.fechaFinalización = fechaFinalización;
+    }
+
+    public Date getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(Date fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    @Override
+    public String toString() {
+        return estado;
+    }
+}
--- src/main/java/jin/com/edu/ordenesservicios/modificarController.java
+++ src/main/java/jin/com/edu/ordenesservicios/modificarController.java
@@ -1,4 +1,79 @@
 package jin.com.edu.ordenesservicios;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class modificarController {
+    //Variables Gráficas
+
+    @FXML Label labNoServicio;
+    @FXML Label labFregistro;
+    @FXML TextField txtTE;
+    @FXML ComboBox<Personal> cbOperadores;
+    @FXML ComboBox cbestatus;
+    @FXML DatePicker dpFinalizacion;
+    @FXML TextArea txaDescripcion;
+    @FXML TextArea txaObservaciones;
+
+    //Varibles de Programación
+    private ObservableList<Personal> personalLista;
+    private ObservableList<Tarea> estado;
+    private int id;
+    public void setId(int id) {
+        this.id = id;
+    }
+    @FXML
+    public void datosCombo(){
+        try {
+            Connection c = EnlaceIvan.getConexion();
+            Statement stm = c.createStatement();
+            String sql = "SELECT * FROM personal";
+            ResultSet r = stm.executeQuery(sql);
+            personalLista.clear();
+            while (r.next()) {
+                personalLista.add(new Personal(r.getInt("id"), r.getString("areaConocimiento"),
+                        r.getInt("edad"), r.getString("nombre"), r.getString("sexo").charAt(0)));
+            }
+            stm.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+    @FXML
+    public void modifcarOrden(){
+        //System.out.println(cbestatus.getSelectionModel().getSelectedItem().toString().charAt(0));
+
+        try{
+            Connection c = EnlaceIvan.getConexion();
+            Statement stm = c.createStatement();
+            String sql = "UPDATE tareaservico SET status = '"+cbestatus.getSelectionModel().getSelectedItem().toString().charAt(0)+"',fecha = '"+dpFinalizacion.getValue()+"',observacion = '"+txaObservaciones.getText()+"',tiempoestimado = '"+txtTE.getText()+"',solicitud = '"+labNoServicio.getText()+"', personal = '"+cbOperadores.getSelectionModel().getSelectedItem().toString2()+"' WHERE id = '"+labNoServicio.getText()+"';";
+            System.out.println(stm.executeUpdate(sql));
+            stm.close();
+        }catch (Exception e){
+            System.out.println(e);
+            System.out.println("NO FUNCIONA LA CONSULTA");
+
+
+        }
+
+    }
+    public void initialize() {
+        personalLista = FXCollections.observableArrayList();
+        estado = FXCollections.observableArrayList();
+        estado.add(new Tarea("En Espera"));
+        estado.add(new Tarea("Atendido"));
+        estado.add(new Tarea("No Atendido"));
+        datosCombo();
+        cbOperadores.setItems(personalLista);
+        cbestatus.setItems(estado);
+
+    }
+
 }
